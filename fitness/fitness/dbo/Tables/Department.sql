dfd14788e31684a6fac9a58bd8a6dc2951f150f9
Vania <kiievets1ivan@hneu.net>
2019-08-08 16:48:49 +0300

    add tables

---
+++ fitness/fitness/dbo/Tables/Department.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Department]
+(
+	[Id] INT NOT NULL IDENTITY (1,1) PRIMARY KEY, 
+    [Name] NVARCHAR(50) NULL, 
+    CONSTRAINT [FK_Department_ToEmployee] FOREIGN KEY (Id) REFERENCES Employee(Department_Id)
+)
